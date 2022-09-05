2ca6bc7aa2b37314ed4f00d313c6d064717fc328
anthonytrillo <anthony.trillo.92@gmail.com>
2022-09-05 19:38:15 -0300

    Added index authentication

--- routes/router.js
+++ routes/router.js
@@ -1,22 +1,26 @@
 const express = require('express');
 const router = express.Router();
 
-const db = require('../database/db');
 const registerController = require('../controllers/registerController');
+const loginController = require('../controllers/loginController');
+const authenticationController = require('../controllers/authenticationController');
+const logoutController = require('../controllers/logoutController');
 
-router.get('/', (req, res) => {
-    res.render('index');
+router.get('/', authenticationController.isAuthenticated, (req, res) => {
+    res.render('index', { user: req.user });
 });
 
 router.get('/login', (req, res) => {
-    res.render('login');
+    res.render('login', { alert: false });
 });
 
 router.get('/register', (req, res) => {
     res.render('register');
 });
 
-// Routes for methods controller\
+// Routes for methods controller
 router.post('/register', registerController.register);
+router.post('/login', loginController.login);
+router.get('/logout', logoutController.logout);
 
 module.exports = router;
