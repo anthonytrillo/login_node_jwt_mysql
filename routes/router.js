e082a691bd8eb2e2e03baffbf3121f80849ac72d
anthonytrillo <anthony.trillo.92@gmail.com>
2022-09-05 09:01:10 -0300

    Add the register path

--- routes/router.js
+++ routes/router.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const db = require('../database/db');
+const registerController = require('../controllers/registerController');
 
 router.get('/', (req, res) => {
     res.render('index');
@@ -14,5 +15,8 @@ router.get('/login', (req, res) => {
 router.get('/register', (req, res) => {
     res.render('register');
 });
+
+// Routes for methods controller\
+router.post('/register', registerController.register);
 
 module.exports = router;
